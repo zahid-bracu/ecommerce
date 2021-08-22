7c9ce1bf801dea8b552710fcb6fce5298a121a5c
zahid-bracu <zahid.mym@gmail.com>
2021-08-22 22:21:09 +0600

    ok

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const fs=require('fs');
 const app = express()
 const port = 3100;
 
 app.get('/', (req, res) => {
   res.send({name:"zahid"})
+})
+
+
+app.get('/data',(req,res)=>{
+  
 })
 
 app.listen(port, () => {
